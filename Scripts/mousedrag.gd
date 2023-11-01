163514da8bff0b56484c81d0bc74b9a29c2ae52c
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2023-11-01 19:22:42 +0100

    Update mousedrag.gd

--- Scripts/mousedrag.gd
+++ Scripts/mousedrag.gd
@@ -10,11 +10,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("Left-click"):
-		print("hoslis")
-		print("la posicion de la carta es:  " ,position)
-		print("la posicion del cursor es: ", get_local_mouse_position())
 		if (get_local_mouse_position().x > -30 and get_local_mouse_position().x < 30 and get_local_mouse_position().y < 40 and get_local_mouse_position().y > -40):
 				position = get_global_mouse_position()
-
+	
 
 
