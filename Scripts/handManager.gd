6374730192f3e5ae7f1c0c8d776d551b731bc7aa
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2023-11-03 14:04:54 +0100

    nada

--- Scripts/handManager.gd
+++ Scripts/handManager.gd
@@ -6,13 +6,20 @@ var possize = vectorPosiciones.size()
 
 
 func _ready():
-	for n in possize:
-		vectorPosiciones[n].xposition = vectorPosiciones[n].xposition + (100*n)
-		print(vectorPosiciones[n].xposition)
-	print(vectorPosiciones[0].xposition)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Actualizo el tamaño del vector en caso de que se creen cartas nuevas
 	possize = vectorPosiciones.size()
 	
+
+
+func _on_carta_locatespot():
+	print("Señal recibida")
+	for n in possize:
+		vectorPosiciones[n].xposition = vectorPosiciones[n].xposition + (100*n)
+		print(vectorPosiciones[n].xposition)
+	print(vectorPosiciones[0].xposition)
+
+
