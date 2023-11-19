8a48fbaab259b6d63b372e7f383e98826fa216c3
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2023-11-19 16:31:09 +0100

    deja carta medion

--- Scripts/crearCartasAMano.gd
+++ Scripts/crearCartasAMano.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var cartaIns = preload("res://Escenas/carta.tscn")
+
+var cont = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -8,6 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_just_pressed("Space")):
-		print("Espacio pulsado")
+	if (cont < 13):
+		cont += 1 # cont = cont + 1
+		print("Contador: ", cont)
 		add_child(cartaIns.instantiate())
