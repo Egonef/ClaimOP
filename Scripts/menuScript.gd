e83f3f25c62b5630c704d9e6b277543d1650535f
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2023-12-31 20:57:16 +0100

    conexion rpc

--- Scripts/menuScript.gd
+++ Scripts/menuScript.gd
@@ -21,3 +21,7 @@ func _on_opciones_pressed():
 
 func _on_salir_pressed():
 	get_tree().quit()
+
+
+func _on_multijugador_pressed():
+	get_tree().change_scene_to_file("res://Escenas/menumulti.tscn")
---
+++ Scripts/multiplayermain.gd
@@ -0,0 +1,61 @@
+extends Node
+
+@export var address = "127.0.0.1"
+@export var port = 8081
+
+var peer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	multiplayer.peer_connected.connect(JugadorConectado)
+	multiplayer.peer_disconnected.connect(JugadorDesconectado)
+	multiplayer.connected_to_server.connect(JugadorConectadoServer)
+	multiplayer.connection_failed.connect(JugadorDesconectadoServer)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func JugadorConectado(id):
+	print("Jugador conectao: " + str(id))
+
+func JugadorDesconectado(id):
+	print("Jugador desconectao: " + str(id))
+
+func JugadorConectadoServer():
+	print("Conectao al servidor")
+
+func JugadorDesconectadoServer():
+	print("Conexion fallida con el servidor")
+
+@rpc("any_peer","call_local")
+func entrar_en_partida():
+	var escena = load("res://Escenas/Tablero.tscn").instantiate()
+	get_tree().root.add_child(escena)
+	self.visible = false
+
+func _on_crear_pressed():
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(port,2)
+	if error != OK:
+		print("No se ha podido crear la partida. Codigo de error:" + error)
+		return
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	
+	multiplayer.set_multiplayer_peer(peer)
+	print("Partida creada con éxito, esperando jugadores.")
+
+func _on_unirse_pressed():
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(address,port)
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(peer)
+
+func _on_volver_pressed():
+	get_tree().change_scene_to_file("res://Escenas/menu.tscn")
+
+
+func _on_iniciar_partida_pressed():
+	entrar_en_partida.rpc()
